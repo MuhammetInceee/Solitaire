d62fe6c8bae8ee023600d1ccd0e5a9e3a29a4743
MuhammetInce <muhammetinceee@gmail.com>
2021-09-16 15:17:43 +0300

    + Add : Sprite Update For Card Flipper

---
+++ Assets/Scripts/SpriteUpdate.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteUpdate : MonoBehaviour
+{
+    public Sprite _cardFace;
+    public Sprite _cardBack;
+
+    private SpriteRenderer _spriteRenderer;
+    private Selectable _selectable;
+    private DeckManager _deckManager;
+
+    void Start()
+    {
+        List<string> deck = DeckManager.GenerateDeck();
+        _deckManager = FindObjectOfType<DeckManager>();
+
+        int i = 0;
+        foreach(string card in deck)
+        {
+            if(this.name == card)
+            {
+                _cardFace = _deckManager.cardFaces[i];
+                break;
+            }
+            i++;
+        }
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _selectable = GetComponent<Selectable>();
+    }
+
+
+    void Update()
+    {
+        if(_selectable.faceUp == true)
+        {
+            _spriteRenderer.sprite = _cardFace;
+        }
+        else if(_selectable.faceUp == false)
+        {
+            _spriteRenderer.sprite = _cardBack;
+        }
+    }
+}
